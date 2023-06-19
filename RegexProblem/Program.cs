0386bcd710eaa7f1a487700bd48979644a1cb35d
Poojachakor <chakorpooja07@gmail.com>
2023-06-19 18:03:14 +0530

    Add: UC6 file

--- RegexProblem/Program.cs
+++ RegexProblem/Program.cs
@@ -6,24 +6,28 @@ namespace RegexProblem
     {
         static void Main(string[] args)
         {
+            string websiteAddress = "https://www.example.com";
 
-            string[] fileNames = { "image.jpg", "photo.png", "document.docx", "picture.gif", "data.csv" };
-            string pattern = @"^.+\.(jpg|jpeg|png|gif)$";
-
-            foreach (string fileName in fileNames)
+            bool isValid = ValidateWebsiteAddress(websiteAddress);
+            if (isValid)
             {
-                if (Regex.IsMatch(fileName, pattern, RegexOptions.IgnoreCase))
-                {
-                    Console.WriteLine($"{fileName} is a valid image file name.");
-                }
-                else
-                {
-                    Console.WriteLine($"{fileName} is not a valid image file name.");
-                }
+                Console.WriteLine("Website address is valid.");
             }
+            else
+            {
+                Console.WriteLine("Invalid website address format.");
+            }
+        }
+
+        static bool ValidateWebsiteAddress(string websiteAddress)
+        {
+            
+            string pattern = @"^(https?://)?([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+(/\S*)?$";
+
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(websiteAddress);
         }
     }
 }
 
-
-    
+            
