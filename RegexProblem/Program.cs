a39e6fdab1db1e9eb8411ef72c9aadb2b84f53ba
Poojachakor <chakorpooja07@gmail.com>
2023-06-19 17:24:10 +0530

    Add: RegexProblem

---
+++ RegexProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string input = "abb";
+
+            
+            string pattern = @"a{1}b{2,3}";
+
+            
+            Regex regex = new Regex(pattern);
+
+            
+            if (regex.IsMatch(input))
+            {
+                Console.WriteLine("String matched the pattern!");
+            }
+            else
+            {
+                Console.WriteLine("String did not match the pattern!");
+            }
+        }
+    }
+}
+    
